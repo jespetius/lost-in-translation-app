2b8ffa57ecebe1d9f99c8f6f1fa59d64e6be43e1
jesperi.kuula <jeppe.kuula@gmail.com>
2023-01-24 16:05:44 +0200

    feat: basic user actions, logout and moving to translater

--- src/components/Profile/ProfileActions.jsx
+++ src/components/Profile/ProfileActions.jsx
@@ -1,7 +1,26 @@
-const ProfileActions =() => {
+import {Link } from 'react-router-dom';
+import { STORAGE_KEY_USER } from '../../const/storageKeys';
+import { useUser } from '../../context/UserContext';
+import { storageDelete} from '../../utils/storage';
+
+
+const ProfileActions = () => {
+
+    const { setUser } = useUser()
+    
+    const handleLogoutClick = () => {
+        if (window.confirm("Are you sure?")){
+            storageDelete(STORAGE_KEY_USER)
+            setUser(null)
+        }
+    }
+
     return (
-        <p>Actions</p>
+        <ul>
+            <li><Link to="/translate">Translate</Link></li>
+            <li><button>Clear History</button></li>
+            <li><button onClick={ handleLogoutClick}>Log Out</button></li>
+        </ul>
     )
 }
-
 export default ProfileActions
