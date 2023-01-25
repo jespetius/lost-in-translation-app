4c7046af5bf9302728d99f20282ed7c69e2aa4ec
kouzber123 <tom.nguyen@windowslive.com>
2023-01-25 16:50:05 +0200

    feature: Add translation feature User can translate words

--- src/components/Profile/ProfileActions.jsx
+++ src/components/Profile/ProfileActions.jsx
@@ -1,13 +1,14 @@
 import { STORAGE_KEY_USER } from '../../const/storageKeys';
 import { useUser } from '../../context/UserContext';
-import { storageDelete} from '../../utils/storage';
+import { storageDelete, storageSave} from '../../utils/storage';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { ClearTranslationHistory } from '../../api/translation';
 
 //profile actions
 const ProfileActions = () => {
     //to get current user
-    const { setUser } = useUser()
+    const { user ,setUser } = useUser()
     //log out button for user by deleting local storagekey
     const handleLogoutClick = () => {
         if (window.confirm("Are you sure?")){
@@ -15,11 +16,31 @@ const ProfileActions = () => {
             setUser(null)
         }
     }
+
+    const clearHistory = async () => {
+        if (!window.confirm("Are you sure?\n this can not be undone")) {
+          return;
+        }
+        const [clearError] = await ClearTranslationHistory(user.id);
+        if (clearError !== null) {
+          return [clearError, null];
+        }
+        console.log(clearError);
+    
+        const updatedUser = {
+          ...user,
+          translations: []
+        };
+    
+        console.log(updatedUser);
+        storageSave(STORAGE_KEY_USER, updatedUser);
+        setUser(updatedUser);
+      };
     //return user action buttons
     return (
         <ButtonGroup vertical>
             <Button variant="warning" href='/translate'>Translate</Button>
-            <Button variant="danger">Clear History</Button>
+            <Button variant="danger" onClick={clearHistory}>Clear History</Button>
             <Button variant="danger"  onClick={ handleLogoutClick}>Log Out</Button>
             </ButtonGroup>
     )
--- src/views/Translate.jsx
+++ src/views/Translate.jsx
@@ -1,8 +1,48 @@
-import withAuth from "../hoc/withAuth"
+import { useState } from "react";
+import { addTranslation } from "../api/translation";
+import TranslationForm from "../components/Translations/TranslationForm";
 
+
+import { STORAGE_KEY_USER } from "../const/storageKey";
+import { TRANSLATION } from "../const/translations";
+import { useUser } from "../context/UserContext";
+import withAuth from "../hoc/withAuth";
+import { storageSave } from "../utils/storage";
 const Translate = () => {
+  //this handles the values
+
+  const { user, setUser } = useUser();
+  const [images, setImage] = useState(null);
+  //states
+  const [translationText, setTranslationText] = useState(null);
+
+  //translationtext is string user gave
+  const handleUserInput = async translationText => {
+    setTranslationText(translationText);
+    const createArr = translationText.toLowerCase().replace(/\s/g, "").split("");
+    setImage(createArr.map(letter => TRANSLATION.find(l => l.letter === letter).image));
+    const transtext = translationText.trim();
+    const [error, updatedUser] = await addTranslation(user, transtext);
+
+    if (error !== null) {
+      return;
+    }
+
+    storageSave(STORAGE_KEY_USER, updatedUser);
+    setUser(updatedUser);
+    //we should update the state
+    console.log("err ", error);
+    console.log("result ", updatedUser);
+  };
+  const CreateImages = () => images.map((img, index) => <img key={index} src={`/individial_signs/${img}`} alt={img} height="50" />);
   return (
-    <h1> Translate </h1>
-  )
-}
-export default withAuth(Translate)
+    <div className="translation-form">
+      <TranslationForm handleUserInput={handleUserInput} />
+      <h4>result:</h4>
+      {translationText && <p>word is {translationText}</p>}
+      {images && <CreateImages />}
+    </div>
+  );
+};
+
+export default withAuth(Translate);
