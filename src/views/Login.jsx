abe62d0e12556021b7fb7eb10a1e16deab7a2f50
jesperi.kuula <jeppe.kuula@gmail.com>
2023-01-26 16:46:19 +0200

    feat: Loginpage style upgrade

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,9 +2,13 @@ import LoginForm from '../components/Login/LoginForm'
 const Login = () => {
   return (
     <>
+    <div class="login">
       <img src ="resources\LostInTranslation_Resources\Logo.png" alt="splash" width="155"/>
       <h1> Login </h1>
-      <LoginForm/>
+      <div class="translation-list">
+        <LoginForm/>
+      </div>
+    </div>
     </>
   )
 }
